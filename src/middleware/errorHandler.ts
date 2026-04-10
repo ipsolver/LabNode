90d32b0c4af6a10096f6d5beb2e3b7da5cc07e4a
Vadim Lishchynskyi <ipzk241_lvv@student.ztu.edu.ua>
2026-04-10 13:45:09 +0300

    Added error handling for mongo

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,4 +1,6 @@
 import { ZodError } from 'zod';
+import mongoose from 'mongoose';
+import { MongoServerError } from 'mongodb';
 import { Request, Response, NextFunction } from 'express';
 
 export function errorHandler(
@@ -13,6 +15,28 @@ export function errorHandler(
         field: i.path.join('.'),
         message: i.message,
       })),
+    });
+  }
+
+  if (err instanceof mongoose.Error.CastError) {
+    return res.status(400).json({
+      message: `Invalid ${err.path}`,
+    });
+  }
+
+  if (err instanceof mongoose.Error.ValidationError) {
+    return res.status(400).json({
+      errors: Object.values(err.errors).map((error) => ({
+        field: error.path,
+        message: error.message,
+      })),
+    });
+  }
+
+  if (err instanceof MongoServerError && err.code === 11000) {
+    return res.status(409).json({
+      message: 'Duplicate key error',
+      keyValue: err.keyValue,
     });
   }
 
