910d593edca4ca98874d3f6e069bc94bfc3a90da
Vadim Lishchynskyi <ipzk241_lvv@student.ztu.edu.ua>
2026-05-08 12:58:44 +0300

    Test failed healthcheck deploy

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const PORT = Number(process.env.PORT) || 3000;
 const HOST = '0.0.0.0';
 
-async function start() {
+async function start() { 
   const server = app.listen(PORT, HOST, () => {
     console.log(`Server running on http://${HOST}:${PORT}`);
   });
